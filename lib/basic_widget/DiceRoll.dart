e1187100dfd243156d5016299423994042472fdf
Shlok Aggrawal <109579954+Shlok-Android-Dev@users.noreply.github.com>
2024-11-25 15:12:46 +0530

    Dice Roll complete

--- lib/basic_widget/DiceRoll.dart
+++ lib/basic_widget/DiceRoll.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class DiceRoll extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_basics_widget/Utils/Utils.dart';
+
+class DiceRoll extends StatefulWidget {
   final Color color;
   final IconData icon;
   final int index;
@@ -8,33 +11,98 @@ class DiceRoll extends StatelessWidget {
   DiceRoll({required this.color, required this.icon, required this.index});
 
   @override
+  State<DiceRoll> createState() => _DiceRollState();
+}
+
+class _DiceRollState extends State<DiceRoll> {
+  int leftButton = 1;
+  int rightButton = 1;
+  int topDice = 1;
+
+  void topDiceNumber() {
+    setState(() {
+      topDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  void changeDiceNumber() {
+    setState(() {
+      leftButton = Random().nextInt(6) + 1;
+      rightButton = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext coantext) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.yellow,
         appBar: AppBar(
-          title: Text(
-            'Dice Roll',
+          title: Center(
+            child: Text(
+              'Dice Roll',
+              style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
           backgroundColor: Colors.yellow,
         ),
-        body: Center(
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Expanded(
-                  child: Image.asset('images/dice1.png'),
-                ),
+        body: Column(
+          children: [
+            Text(
+              'Single Dice',
+              style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Container(
+              margin: EdgeInsets.all(15.0),
+              child: TextButton(
+                onPressed: () {
+                  Utils.toastMessage('left dice');
+                  topDiceNumber();
+                },
+                child: Image.asset('images/dice$topDice.png'),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Expanded(
-                  child: Image.asset('images/dice6.png'),
-                ),
-              )
-            ],
-          ),
+            ),
+            Text(
+              'Doubble Dice',
+              style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Center(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {
+                        Utils.toastMessage('left dice');
+                        changeDiceNumber();
+                      },
+                      child: Image.asset('images/dice$leftButton.png'),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {
+                        changeDiceNumber();
+
+                        Utils.toastMessage('right dice');
+                        leftButton = Random().nextInt(6) + 1;
+                        rightButton = Random().nextInt(6) + 1;
+                      },
+                      child: Image.asset('images/dice$rightButton.png'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
